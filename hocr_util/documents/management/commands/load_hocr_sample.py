856e9ae032fa4f6fe0e836c671e5374445a5ef30
Jacob Fenton <jfenton@sunlightfoundation.com>
2014-02-23 07:47:28 -0500

    complain loudly if sample hocr docs are not found

--- hocr_util/documents/management/commands/load_hocr_sample.py
+++ hocr_util/documents/management/commands/load_hocr_sample.py
@@ -1,6 +1,7 @@
 """ 
 Try loading a 1000-document hocr sample into the db. 
-    assumes that the 1000-file hocr sample is loaded into the SAMPLE_FILE_DIR
+Assumes that the 1000-file hocr sample is loaded into the SAMPLE_FILE_DIR
+
 """
 import os 
 
@@ -17,23 +18,26 @@ SAMPLE_FILE_DIR = 'parser/hocr_sample/'
 # Because this could take seriously long
 FILE_ENTRY_CAP = 1000
 
+warning_message = """
+No html files were found to enter. You must download the hocr sample and unzip
+it, then set that directory as the SAMPLE_FILE_DIR in the body of this command.
+The sample files are not included as part of the repo, but are available here:
+http://pdf-liberation.s3.amazonaws.com/hocr_sample.zip
+"""
+
 class Command(BaseCommand):
     help = "test by loading a document"
     requires_model_validation = False
 
     def handle(self, *args, **options):
         
-        # just try one that's weird.
-        if False:
-            file_path = "parser/hocr_sample/02-0575282_990EZ_201212.html"
-            doc_id = "02-0575282_990EZ_201212"
-            enter_document(file_path, doc_id)
-            assert False
 
         start = datetime.now()
-        
+        files_found = 0
         for d, _, files in os.walk(SAMPLE_FILE_DIR):
+            
             for i, this_file in enumerate(files):
+                files_found += 1
                 file_path = SAMPLE_FILE_DIR + this_file
                 # ignore files that aren't .html in case any got mixed in there
                 # may want to filter on other criteria here too
@@ -49,6 +53,10 @@ class Command(BaseCommand):
                         print "Error entering file number %s from %s - entire document skipped. " % (i, file_path)
                         print traceback.print_exc()
         
-        end = datetime.now()
-        elapsed = end-start
-        print "Time elapsed: %s" % (elapsed)
+        if files_found ==0:
+            print warning_message
+            
+        else:
+            end = datetime.now()
+            elapsed = end-start
+            print "Time elapsed: %s" % (elapsed)
