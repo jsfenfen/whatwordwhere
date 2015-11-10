004673d6f2549c97646558933c6aa82636f8bc58
Jacob Fenton <jfenton@sunlightfoundation.com>
2015-11-10 09:43:19 -0800

    django rest framework gis working w/ single polygons

--- hocr_util/api/serializers.py
+++ hocr_util/api/serializers.py
@@ -1,9 +1,19 @@
 from documents.models import Page
-from rest_framework import serializers
+from rest_framework_gis import serializers
 
 
+"""
 class PageSerializer(serializers.ModelSerializer):
    
     class Meta:
         model = Page
         fields = ('page_number', 'image', 'thumbnail', 'doc')
+
+"""
+
+class PageSerializer(gis_serializers.GeoFeatureModelSerializer):
+
+    class Meta:
+        model = Page
+        geo_field = "page_dimensions"
+        fields = ('page_number', 'image', 'thumbnail', 'doc', 'page_dimensions')
